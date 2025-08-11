ad7660e44d4e3279200ee463409dbd11d3d1f78a
mohammad riyazuddin <mohammadriyazuddin494@gmail.com>
2025-08-11 22:14:14 +0530

    add the register user controller

--- src/controller/registeruser.controller.js
+++ src/controller/registeruser.controller.js
@@ -2,26 +2,31 @@ import {User} from '../model/user.model.js'
 
 const registerUser= async(req,res)=>{
     try{
+        //get user detail from req.body
         const {username,email,fullname,password}=req.body;
 
+        // validate that any filed is not empty in case
         if(!username || !email || !fullname || !password){
             return res
             .status(400)
             .json({message:"All field are required"});
         }
+
+        //checking if user already exist
         const existingUser=await User.findOne({email});
         if(existingUser){
             return res
             .status(409)
             .json({message:"username or Email already exist"});
         }
+        // creating a user and password will be hashed
         const user= await User.create({
             fullname,
             email,
             username,
             password
         });
-
+        //removing password from the response
         const createUser= await User.findById(user._id).select
         ("-password");
 
