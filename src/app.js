1033d634d9a6f94ffe726fa301a8c0eb40f409bc
mohammad riyazuddin <mohammadriyazuddin494@gmail.com>
2025-08-17 20:54:36 +0530

    added the app.js

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import cookieparser from "cookie-parser";
+
+const app=express();
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
+
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.static("public"));
+app.use(cookieparser());
+
+import userRouter from "./routes/user.router.js";
+
+app.use("/api/v1/users",userRouter);
+
+
+export {app}
